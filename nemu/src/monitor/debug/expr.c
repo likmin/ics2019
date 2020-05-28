8417845870fe7c1a2161075be30effedf1c26abc
likai <likmi>
2020-05-28 23:22:04 +0800

    test commit

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -147,6 +147,7 @@ static bool make_token(char *e) {
       }
     }
 
+
     if (i == NR_REGEX) {
       printf("no match at position %d\n%s\n%*.s^\n", position, e, position, "");
       return false;
