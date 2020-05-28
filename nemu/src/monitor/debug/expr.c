d4cdf7745525cc4021fe92ba3f32eb05cdc5cff4
tracer-ics2019 <tracer@njuics.org>
2020-05-28 22:33:15 +0800

    >  compile 181220000 likai Linux likai 5.3.0-53-generic #47~18.04.1-Ubuntu SMP Thu May 7 13:10:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  22:33:15 up  4:25,  3 users,  load average: 0.06, 0.05, 0.04 3dc35ba5a5e3553260f305299e281de65f382b4f

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -36,7 +36,7 @@ static struct rule {
   /* TODO: why is \\- nor \- ? '\' is Escape String */
  // {"^\\$e*[acdb][x|l|h]|^\\$e*[sb][p|h]|^\\$e*[sd][i|h]", TK_REG}, // regex for x86 register
   {"[0-9]+", TK_DNUM}, // decimal-number
- // {"0[xX][0-9a-fA-F]+",TK_HNUM}, // hexadecimal-number
+  {"0[xX][0-9a-fA-F]+",TK_HNUM}, // hexadecimal-number
   
   {"-", '-'},			// sub
   {"\\*", '*'},			// mul
@@ -161,7 +161,7 @@ uint32_t expr(char *e, bool *success) {
  }
   
   /* TODO: Insert codes to evaluate the expression. */
-  TODO();
+  // TODO();
 
   return 0;
 }
