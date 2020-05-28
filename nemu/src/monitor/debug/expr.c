e5fe9daf54309cb236aaffc3be711336ccec9c1e
tracer-ics2019 <tracer@njuics.org>
2020-05-28 22:22:10 +0800

    >  compile 181220000 likai Linux likai 5.3.0-53-generic #47~18.04.1-Ubuntu SMP Thu May 7 13:10:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  22:22:10 up  4:14,  3 users,  load average: 0.21, 0.12, 0.06 6494177bffe5ac3626452c00c6f0b531a5ecf7a

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -34,10 +34,10 @@ static struct rule {
   {"==", TK_EQ},        // equal
 
   /* TODO: why is \\- nor \- ? '\' is Escape String */
-/*  {"^\\$e*[acdb][x|l|h]|^\\$e*[sb][p|h]|^\\$e*[sd][i|h]", TK_REG}, // regex for x86 register
+ // {"^\\$e*[acdb][x|l|h]|^\\$e*[sb][p|h]|^\\$e*[sd][i|h]", TK_REG}, // regex for x86 register
   {"/\b[0-9]+\b/", TK_DNUM}, // decimal-number
   {"\b0[xX][0-9a-fA-F]+\b",TK_HNUM}, // hexadecimal-number
-  */
+  
   {"-", '-'},			// sub
   {"\\*", '*'},			// mul
   {"\\/", '/'},			// div
