3507bcd36e9333a8a4583e382c2e7e5c1bf49217
tracer-ics2019 <tracer@njuics.org>
2020-05-28 22:57:04 +0800

    >  compile 181220000 likai Linux likai 5.3.0-53-generic #47~18.04.1-Ubuntu SMP Thu May 7 13:10:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  22:57:04 up  4:49,  3 users,  load average: 0.06, 0.06, 0.04 e35866a22dd994cbb1b80fcce74c35a862ddace5

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -88,6 +88,10 @@ static bool make_token(char *e) {
   regmatch_t pmatch;
 
   nr_token = 0;
+  /* begin to make token, you should flush the tokens, or next time if may going wrong */
+  int j;
+  for (j = 0;j < NR_REGEX; j ++) 
+	  memset(tokens[j].str, '\0', 32);
 
   while (e[position] != '\0') {
 
