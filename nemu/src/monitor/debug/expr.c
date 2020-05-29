d13d87b0a61e7bb88f253a8bed0cef98605b581b
tracer-ics2019 <tracer@njuics.org>
2020-05-29 23:26:47 +0800

    >  compile 181220000 likai Linux likai 5.3.0-53-generic #47~18.04.1-Ubuntu SMP Thu May 7 13:10:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  23:26:47 up  1:48,  3 users,  load average: 0.44, 0.51, 0.22 cc6fe74f7fad8b9e4c95359ab350bb43a1b744d2

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -159,7 +159,7 @@ static bool make_token(char *e) {
 bool check_parenteses(uint32_t p, uint32_t q) {
 	int flag = 0;
 	
-	while(p < q) {
+	while(p <= q) {
 		printf("[check_parenteses] p = %u, q = %u,flag = %d\n", p,q,flag);
 		if(strcmp(tokens[p].str,"(") == 0) {
 			p++;
