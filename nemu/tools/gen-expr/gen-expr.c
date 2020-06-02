67566ef8c02f20d510a1633ca46d2fead7f60ef3
likai <likmi>
2020-06-02 16:56:48 +0800

    [pa1] complete the part 2

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -5,30 +5,71 @@
 #include <assert.h>
 #include <string.h>
 
-char *getnum() {
-	srand(time(NULL));
-	uint32_t num = 	rand();
-	char *Num;
-	utoa(num, Num);
-	return Num;
-}
 // this should be enough
 static char buf[65536];
+
+/*
+ * inline expansion, or inlining, is a manual or compiler optimization that
+ * replaces a function call site with the body of the called function
+ * 
+ * it's faster than call function
+ */
+uint32_t choose(uint32_t n) {
+	return rand() % n;	
+}
+
+static inline void gen_num() {
+	char s[4];
+	uint32_t n = choose(99);
+	
+	/* send the formatted data to string */
+	sprintf(s, "%u", n);
+	strcat(buf, s);		
+}
+
+static inline void gen(char str) {
+	/* generate random white space */
+	uint32_t lSpace = choose(4);
+	uint32_t rSpace = choose(4);
+	
+	char s[lSpace + 1 + rSpace];
+
+	uint32_t i;
+
+	for (i = 0; i < lSpace; i++) s[i] = ' ';
+	s[i++] = str;
+	for (;i < lSpace + 1 + rSpace; i++) s[i] = ' ';
+    s[lSpace + 1 + rSpace] = '\0';	
+	strcat(buf, s);
+
+}
+
+static inline void gen_rand_op() {
+
+	switch (choose(4)) {
+		case 0: gen('+'); break;
+		case 1: gen('-'); break;
+		case 2: gen('*'); break;
+		case 3: gen('/'); break;
+	}	
+}
+
 static inline void gen_rand_expr() {
   
-  switch (rand() % 3) {
-	case 0: getnum();
-	case 1: 
-	default:  
+  switch(choose(3)) {
+	case 0: gen_num(); break;
+	case 1: gen('('); gen_rand_expr(); gen(')'); break;
+	default:  gen_rand_expr(); gen_rand_op(); gen_rand_expr(); break; 
   
   }
+
 }
 
 static char code_buf[65536];
 static char *code_format =
 "#include <stdio.h>\n"
 "int main() { "
-"bool flag;"
+"_Bool flag;"
 "  unsigned result = %s; "
 "  printf(\"%%u\", result); "
 "  return 0; "
@@ -60,10 +101,12 @@ int main(int argc, char *argv[]) {
 
     int result;
     int fsn = fscanf(fp, "%d", &result);
-    printf("fsn = %d\n", fsn);
+	printf("[loop %d] fsn = %d\t ", i, fsn); /* TODO: delete fsn and printf it */
     pclose(fp);
 
-    printf("%u %s\n", result, buf);
+    printf("%u\t %s\n", result, buf);
+  
+	memset(buf, '\0', 65536);
   }
   return 0;
 }
