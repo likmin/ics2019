2b27a11baeef246144ca041ab5efb816990fa028
tracer-ics2019 <tracer@njuics.org>
2020-06-03 22:56:52 +0800

    >  compile 181220000 likai Linux likai 5.3.0-53-generic #47~18.04.1-Ubuntu SMP Thu May 7 13:10:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  22:56:52 up 30 min,  3 users,  load average: 0.08, 0.07, 0.08 30c2a7553d084c93e8ab3eba96eb23e15d293df

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -81,7 +81,7 @@ static int cmd_x(char *args) {
 
  if(argN == NULL || argEXPR == NULL) {
 	printf("[x] args error!\n the format of x is the following:\n");
-	cmd_help("help x");
+	cmd_help("x x");
 	return 0;
  }
 
