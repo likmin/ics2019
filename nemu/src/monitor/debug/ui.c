838065d16c660784172da2fd7380a276dc6ae7b4
tracer-ics2019 <tracer@njuics.org>
2020-06-06 10:40:03 +0800

    >  compile 181220000 likai Linux likai 5.3.0-53-generic #47~18.04.1-Ubuntu SMP Thu May 7 13:10:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  10:40:03 up 19 min,  3 users,  load average: 0.10, 0.21, 0.19 88f3c983565713ccea95e053181416c2bfd97501

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -108,7 +108,7 @@ static int cmd_d(char *args) {
 
 	int N = atoi(argN);
 	if (N < 0 || N > 32) {
-		printf("args N is to large!, the range of N is 0 to %d", 32);
+		printf("args N is to large!, the range of N is 0 to %d\n", 32);
 		return 0;
 	}
 
