b5086983ed290f1c056a58ddce7ff9eee8e84249
likai <likmi>
2020-06-22 21:52:13 +0800

    add some white space

--- nemu/src/isa/x86/include/isa/reg.h
+++ nemu/src/isa/x86/include/isa/reg.h
@@ -23,7 +23,7 @@ typedef struct {
 		uint16_t _16;
 		uint8_t _8[2];
 
-	} gpr[8];
+	  } gpr[8];
 
   /* Do NOT change the order of the GPRs' definitions. */
 
@@ -32,7 +32,7 @@ typedef struct {
  
 
    */
-	struct {rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;};
+	  struct {rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;};
   
   };
   vaddr_t pc;
