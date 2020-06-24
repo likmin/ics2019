648f64595895801aa6fa60366a84140ad1ffe0ab
tracer-ics2019 <tracer@njuics.org>
2020-06-24 10:27:56 +0800

    >  compile 181220000 likai Linux likai 5.3.0-59-generic #53~18.04.1-Ubuntu SMP Thu Jun 4 14:58:26 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  10:27:56 up  3:45,  1 user,  load average: 0.00, 0.00, 0.00 6e8ab3bc5b1161f5183efb8c3a84d1b84a7d40

--- nemu/src/isa/riscv32/exec/control.c
+++ nemu/src/isa/riscv32/exec/control.c
@@ -2,7 +2,7 @@
 
 make_EHelper(jal) { /* interpret_rtl_jal(vaddr_t *pc) */
    
-   decinfo.jmp_pc = id_src->val + id_src2->val;
+   rtl_add(&decinfo.jmp_pc, &id_src->val, &cpu.pc);
    printf("jmp_pc = %d\n", decinfo.jmp_pc);
    interpret_rtl_j(decinfo.jmp_pc); 
    print_asm_template1(jal);
