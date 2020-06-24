55253d3a94d603fb608f93c37b9a20dd4c193fc7
tracer-ics2019 <tracer@njuics.org>
2020-06-24 15:01:45 +0800

    >  compile 181220000 likai Linux likai 5.3.0-59-generic #53~18.04.1-Ubuntu SMP Thu Jun 4 14:58:26 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:01:45 up  7:00,  3 users,  load average: 0.00, 0.03, 0.00 71dab0217d156b1d718e749c8fc9681b38101a7

--- nemu/src/isa/riscv32/exec/control.c
+++ nemu/src/isa/riscv32/exec/control.c
@@ -2,8 +2,8 @@
 
 make_EHelper(jal) { /* interpret_rtl_jal(vaddr_t *pc) */
    // rtl_add(&t0, &cpu.pc, 4);
-   printf("[interpret_rtl_jal] pc = 0x%x\n", *pc);
-   printf("[interpret_rtl_jal] pc+ id_src = 0x%x\n", *pc + id_src->val);
+   //printf("[interpret_rtl_jal] pc = 0x%x\n", *pc);
+   //printf("[interpret_rtl_jal] pc+ id_src = 0x%x\n", *pc + id_src->val);
    
    rtl_add(&decinfo.jmp_pc, &id_src->val, &cpu.pc);
    //printf("jmp_pc = 0x%x\n", decinfo.jmp_pc);
@@ -16,5 +16,7 @@ make_EHelper(jalr) {
     t0 = cpu.pc + 4;
     rtl_sr(id_dest->reg, &t0, 4);
     interpret_rtl_j(decinfo.jmp_pc); /* interpret_rtl_jr(&decinfo.jmp_pc); */
-    print_asm_template2(jalr);    
+    print_asm_template2(jalr);   
+
+    printf("[interpret_rtl_jalr] id_src = 0x%x, pc = 0x%x\n",id_src->val, cpu.pc); 
 }
