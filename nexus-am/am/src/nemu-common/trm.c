463ecef622dc3be8853dcbeeba2d738782c4f41c
tracer-ics2019 <tracer@njuics.org>
2020-06-25 20:28:44 +0800

    >  compile 181220000 likai Linux likai 5.3.0-59-generic #53~18.04.1-Ubuntu SMP Thu Jun 4 14:58:26 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  20:28:44 up  5:39,  1 user,  load average: 0.05, 0.06, 0.01 5350be0c569e7ab41f4acef35545ef4a3cd40a28

--- nexus-am/am/src/nemu-common/trm.c
+++ nexus-am/am/src/nemu-common/trm.c
@@ -5,15 +5,18 @@ extern char _heap_start;
 extern char _heap_end;
 int main(const char *args);
 
+/* 用于指示堆区的起始和末尾 */
 _Area _heap = {
   .start = &_heap_start,
   .end = &_heap_end,
 };
 
+/* 用于输出一个字符 */
 void _putc(char ch) {
   outb(SERIAL_PORT, ch);
 }
 
+/* 用于结束程序的运行 */
 void _halt(int code) {
   nemu_trap(code);
 
@@ -21,6 +24,7 @@ void _halt(int code) {
   while (1);
 }
 
+/* 用于进行TRM相关的初始化工作 */
 void _trm_init() {
   extern const char _start;
   const char *mainargs = &_start - 0x100000;
