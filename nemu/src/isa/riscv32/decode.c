c707d46cdf40d6b6a4b3c7a0462d8c3fb9e8330c
tracer-ics2019 <tracer@njuics.org>
2020-06-25 16:21:57 +0800

    >  compile 181220000 likai Linux likai 5.3.0-59-generic #53~18.04.1-Ubuntu SMP Thu Jun 4 14:58:26 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:21:57 up  2:31,  1 user,  load average: 0.07, 0.10, 0.04 1e9d36b5b0fcd24a293773e150158c2e45b2b4

--- nemu/src/isa/riscv32/decode.c
+++ nemu/src/isa/riscv32/decode.c
@@ -118,8 +118,8 @@ make_DHelper(J) { /* void decode_J (vaddr_t *pc), use to decode J-type instructi
 
 make_DHelper(I) {
   int32_t simm = decinfo.isa.instr.simm11_0;
-  simm = simm << 20;
-  simm = simm >> 20;
+  //simm = simm << 20;
+  //simm = simm >> 20;
   printf("[decode_Jalr] simm = 0x%x\n", simm);
   decode_op_r(id_src, decinfo.isa.instr.rs1, true);
   decode_op_i(id_src2, simm, true);
