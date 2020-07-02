8908568915ce8b85c8655868e60774f6fd5d251e
likai <likmi>
2020-07-02 15:33:57 +0800

    try to inplement diff-test

--- nemu/src/isa/riscv32/diff-test.c
+++ nemu/src/isa/riscv32/diff-test.c
@@ -2,9 +2,12 @@
 #include "monitor/diff-test.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
+  
+  int i;
 
+  if (ref_r->pc != pc) return false;
 
-  return false;
+  return true;
 }
 
 void isa_difftest_attach(void) {
