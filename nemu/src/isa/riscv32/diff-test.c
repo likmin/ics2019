b2a69cad6c33a58c81a63bc7b9eedfe5df199156
tracer-ics2019 <tracer@njuics.org>
2020-07-02 22:15:55 +0800

    >  compile 181220000 likai Linux likai 5.3.0-61-generic #55~18.04.1-Ubuntu SMP Mon Jun 22 16:40:20 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  22:15:55 up 4 min,  0 users,  load average: 0.12, 0.19, 0.09 8b72e288fd33d5e72486fa2ee226abb978caff41

--- nemu/src/isa/riscv32/diff-test.c
+++ nemu/src/isa/riscv32/diff-test.c
@@ -4,7 +4,9 @@
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   
   int i;
-
+  for (i = 0; i < 32; i++) {
+    if (ref_r->gpr[i]._32 != reg_l(i)) return false;
+  }
   if (ref_r->pc != pc) return false;
 
   return true;
