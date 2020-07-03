41d0b674257c4b1fd0106116601d2abb3d85d481
tracer-ics2019 <tracer@njuics.org>
2020-07-03 18:01:37 +0800

    >  compile 181220000 likai Linux likai 5.3.0-61-generic #55~18.04.1-Ubuntu SMP Mon Jun 22 16:40:20 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  18:01:37 up 26 min,  3 users,  load average: 0.34, 0.21, 0.29 22087176d6781de95ca1b56bbb6e633f8ae08be0

--- nemu/src/isa/riscv32/diff-test.c
+++ nemu/src/isa/riscv32/diff-test.c
@@ -2,17 +2,24 @@
 #include "monitor/diff-test.h"
 
 
-void difftest_regs_display(CPU_state *ref_r, vaddr_t pc) {
-  int i;
-  for (i = 0; i < 4; i++) printf("Reg.NO \t REF.REG \t REAL.REG \t\t");
-  printf("\n");
-  for (i = 0; i < 32; i++) {
-    printf("%d \t %#X \t %#X \t\t", i, ref_r->gpr[i]._32, reg_l(i));
-    if ((i+1) % 4 == 0) printf("\n"); 
-  }
-  printf("pc = %#X\n", pc);
-}
 
+const char *regsl[] = {
+  "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
+  "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
+  "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
+  "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
+};
+
+void difftest_regs_display(CPU_state *ref_r, vaddr_t pc) {
+
+  int i;
+  for (i = 0; i < 32; i++) {
+    printf("%s = %#X\t", regsl[i], ref_r->gpr[i]._32);
+    if ((i + 1) % 8 != 0) printf(", "); else printf("\n");
+  }
+
+  printf("pc = %#X\n", cpu.pc);
+}
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   
   int i;
