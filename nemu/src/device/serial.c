e7c3fff7d7b69f38694f83ab635da35dc9484cca
tracer-ics2019 <tracer@njuics.org>
2020-07-04 22:05:20 +0800

    >  compile 181220000 likai Linux likai 5.3.0-61-generic #55~18.04.1-Ubuntu SMP Mon Jun 22 16:40:20 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  22:05:20 up  1:20,  6 users,  load average: 0.00, 0.02, 0.01 d65cb334babb5c5bd111842ea5b7dad8c6045bc6

--- nemu/src/device/serial.c
+++ nemu/src/device/serial.c
@@ -3,7 +3,7 @@
 
 /* http://en.wikibooks.org/wiki/Serial_Programming/8250_UART_Programming */
 
-#define SERIAL_PORT 0x3F8
+#define SERIAL_PORT 0x3F8 
 #define SERIAL_MMIO 0xa10003F8
 #define CH_OFFSET 0
 
@@ -24,6 +24,12 @@ static void serial_ch_io_handler(uint32_t offset, int len, bool is_write) {
 
 void init_serial() {
   serial_ch_base = new_space(1);
+
+  /* 注册0x3F8处长度为1字节的端口 */
   add_pio_map("serial", SERIAL_PORT + CH_OFFSET, serial_ch_base, 1, serial_ch_io_handler);
+  /* 注册0xa10003F8处长度为1字节的MMIO空间 */
   add_mmio_map("serial", SERIAL_MMIO + CH_OFFSET, serial_ch_base, 1, serial_ch_io_handler);
+
+  /* TODO: 它们都会映射到串口的数据寄存器，上面两个有啥区别呢？
+   */
 }
--- nexus-am/tests/amtest/src/tests/hello.c
+++ nexus-am/tests/amtest/src/tests/hello.c
@@ -6,6 +6,7 @@ static inline void putstr(const char *s) {
 
 void hello() {
   for (int i = 0; i < 10; i ++) {
-    putstr("Hello, AM World @ " __ISA__ "\n");
+   // putstr("Hello, AM World @ " __ISA__ "\n");
+    printf("Hello world");
   }
 }
