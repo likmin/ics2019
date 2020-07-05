ee2431607034040b6e2e934ca67500f0be56b2ec
tracer-ics2019 <tracer@njuics.org>
2020-07-05 23:12:11 +0800

    >  run 181220000 likai Linux likai 5.3.0-61-generic #55~18.04.1-Ubuntu SMP Mon Jun 22 16:40:20 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  23:12:11 up  5:46,  7 users,  load average: 0.07, 0.04, 0.00 b9f0ecf66d24487a7c7b56da12d4e1e3adcee8f

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -2,6 +2,56 @@
 #include <stdarg.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
+struct param {
+	char lz:1;
+	char zlt:1;
+	char uc:1;
+	char align_left:1;
+	unsigned int width;
+	char sign;
+	unsigned int base;	/* number base, e.g 8, 10, 16*/
+	char *bf;			/* buffer for output */
+};
+void chartobuf(char *p, char c) {
+	*p = c;
+	p++;
+}
+void strtobuf(char *p, char *str) {
+	while(str){
+		*p = *str;
+		p++; str++;
+	}
+}
+
+static void ui2a(unsigned int num, struct param *p)
+{
+    int n = 0;
+    unsigned int d = 1;
+    char *bf = p->bf;
+    while (num / d >= p->base)
+        d *= p->base;
+    while (d != 0) {
+        int dgt = num / d;
+        num %= d;
+        d /= p->base;
+        if (n || dgt > 0 || d == 0) {
+            *bf++ = dgt + (dgt < 10 ? '0' : (p->uc ? 'A' : 'a') - 10);
+            ++n;
+        }
+    }
+    *bf = 0;
+}
+
+static void i2a(int num, struct param *p)
+{
+    if (num < 0) {
+        num = -num;
+        p->sign = '-';
+    }
+    ui2a(num, p);
+}
+
+
 
 int printf(const char *fmt, ...) {
   //for (; *fmt; fmt ++) _putc(*fmt);
@@ -9,15 +59,49 @@ int printf(const char *fmt, ...) {
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
+  char ch;
+	
+	struct param p;
+	char bf[12];
+	p.bf = bf;
+
+	while((ch = *(fmt++))) {
+		if (ch != '%') {
+			chartobuf(out, ch);
+		} else {
+			switch(ch) {
+				case 's':
+					strtobuf(out, va_arg(ap, char *));						
+					break;
+				case 'd':
+					i2a(va_arg(ap, int), &p);				    
+					strtobuf(out, p.bf);
+			 		break;	
+				default: assert(0);
+			}
+		}
+	}
   return 0;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  *out = *fmt;
-  return 0;
+  
+  va_list aptr;
+	int ret;
+	
+	va_start(aptr, fmt);
+	ret = vsprintf(out, fmt, aptr);
+	va_end(aptr);
+	return ret;
 }
 
+/* snprintf()用于将格式化的数据写入字符串 
+ * out: 为要写入的字符串
+ * n  : 要写入的字符的最大数据，超过n会被截断
+ * fmt: 格式化字符串
+ */
 int snprintf(char *out, size_t n, const char *fmt, ...) {
+
   return 0;
 }
 
