defeef0b914ded3c1bab033b086178671200f184
tracer-ics2019 <tracer@njuics.org>
2020-07-06 22:27:13 +0800

    >  run 181220000 likai Linux likai 5.3.0-62-generic #56~18.04.1-Ubuntu SMP Wed Jun 24 16:17:03 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  22:27:13 up  4:00,  1 user,  load average: 0.18, 0.58, 0.62 757317f83a6db396664a47ee1ee287a82c902787

--- nexus-am/tests/amtest/src/tests/hello.c
+++ nexus-am/tests/amtest/src/tests/hello.c
@@ -6,7 +6,8 @@ static inline void putstr(const char *s) {
 
 void hello() {
   for (int i = 0; i < 10; i ++) {
-    putstr("Hello, AM World @ " __ISA__ "\n");
+    //putstr("Hello, AM World @ " __ISA__ "\n");
+    printf("Hello, AM World @ " __ISA__ "\n");
    // printf("Hello World\n");
   }
 }
