e6602522063cf366fa9303bfa95e45db40933bfa
tracer-ics2019 <tracer@njuics.org>
2020-07-07 16:54:26 +0800

    >  run 181220000 likai Linux likai 5.3.0-62-generic #56~18.04.1-Ubuntu SMP Wed Jun 24 16:17:03 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:54:26 up 10:52,  1 user,  load average: 0.05, 0.09, 0.17 7982bd77ad35f6333132b33f9dfedba7a4afca5

--- nexus-am/tests/amtest/src/tests/hello.c
+++ nexus-am/tests/amtest/src/tests/hello.c
@@ -9,7 +9,10 @@ void hello() {
   for (int i = 0; i < 10; i ++) {
     //putstr("Hello, AM World @ " __ISA__ "\n");
     printf("Hello, AM World @ " __ISA__ ",%d, %s \n", i, str);
-    //printf("hello , %s, %d \n",str, i);
+    printf("%s1,%s2\n", str, str);
+    printf("%d, %d \n", i, i+ 1);
+
+    printf("hello , %s, %d \n",str, i);
     //  printf("%s, %d\n", str, i);
     
    // printf("Hello World\n");
