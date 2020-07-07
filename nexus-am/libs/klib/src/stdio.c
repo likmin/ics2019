eafc2501ecf3cc853ea47713a24df0fc75e12a50
tracer-ics2019 <tracer@njuics.org>
2020-07-07 10:27:15 +0800

    >  run 181220000 likai Linux likai 5.3.0-62-generic #56~18.04.1-Ubuntu SMP Wed Jun 24 16:17:03 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  10:27:15 up  6:12,  1 user,  load average: 0.07, 0.06, 0.36 6b24cf4faf2ca5725141b564bcb01eb045f70877

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -2,112 +2,144 @@
 #include <stdarg.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
-struct param {
-	char lz:1;
-	char zlt:1;
-	char uc:1;
-	char align_left:1;
-	unsigned int width;
-	char sign;
-	unsigned int base;	/* number base, e.g 8, 10, 16*/
-	char *bf;			/* buffer for output */
-};
-static inline void chartobuf(char *p, char c) {
-	*p = c;
-	p++;
-}
-static inline void strtobuf(char *p, char *str) {
-	while(str){
-		*p = *str;
-		p++; str++;
+
+char* get_ch(char *dest, int num, int n) {
+	char number[20];
+	for (int j=0;j<10;j++) {number[j]=j+'0';}
+	number[10] = 'a'; number[11] = 'b'; number[12] = 'c'; number[13] = 'd'; number[14] = 'e'; number[15] = 'f';
+	int l=0;
+	int flag=0;
+	if(num==0) {
+		dest[0]='0';
+		dest[1]='\0';
+		return dest;
 	}
+	if(num<0) {
+		flag=1;
+		l++;
+		num=-num;
+	}
+	int temp=num;
+	while(temp>0) {
+		l++;
+		temp=temp/n;
+	}
+	for (int a=0; a<l; a++) {
+		int x=num%n;
+		assert(x<16);
+		dest[l-a-1]=number[x];
+		num=num/n;
+	}
+	if(flag) {dest[0]='-';}
+	dest[l]='\0';
+	return dest;
 }
-
-static inline void putstr(const char *s) {
-  for (; *s; s ++) _putc(*s);
-}
-
-static void ui2a(unsigned int num, struct param *p)
-{
-    int n = 0;
-    unsigned int d = 1;
-    char *bf = p->bf;
-    while (num / d >= p->base)
-        d *= p->base;
-    while (d != 0) {
-        int dgt = num / d;
-        num %= d;
-        d /= p->base;
-        if (n || dgt > 0 || d == 0) {
-            *bf++ = dgt + (dgt < 10 ? '0' : (p->uc ? 'A' : 'a') - 10);
-            ++n;
-        }
-    }
-    *bf = 0;
-}
-
-static void i2a(int num, struct param *p)
-{
-    if (num < 0) {
-        num = -num;
-        p->sign = '-';
-    }
-    ui2a(num, p);
-}
-
-
 
 int printf(const char *fmt, ...) {
-  //for (; *fmt; fmt ++) _putc(*fmt);
-  char p[128]; 
-  
-  va_list va;
-  va_start(va, fmt);
-  vsprintf(p, fmt, va);
-  va_end(va);
-
-  putstr(p);
-  return 0;
+	va_list ap;
+  	va_start(ap,fmt);
+  	int sum=0;
+  	int i=0;
+  	int len=strlen(fmt);
+	while(i<len) {
+    if(fmt[i]=='%') {
+      int num=0;
+      char ls[50]="";
+      switch(fmt[i+1]) {
+        case 'd': {
+	  		num=va_arg(ap,int);
+	  		get_ch(ls, num, 10);
+	  		for(int j = 0; j < strlen(ls); j++) {_putc(ls[j]);}
+	  		sum += strlen(ls);	
+			i+=2;
+        } break;
+		case 's': {
+	  		strcpy(ls,va_arg(ap, char*));
+	  		for(int j = 0; j < strlen(ls); j++) _putc(ls[j]);
+	  		sum += strlen(ls);
+			i+=2;
+		} break;
+		case '0': {
+			num=va_arg(ap,int);
+	  		get_ch(ls, num, 10);
+			int width = fmt[i+2]-'0';
+			for(int i = strlen(ls); i < width; i++) {
+				sum++;
+				_putc('0');
+			}
+	  		for(int j = 0; j < strlen(ls); j++) {_putc(ls[j]);}
+	  		sum += strlen(ls);	
+			i+=4;
+		} break;
+		case 'p': {
+				num=va_arg(ap, int);
+				get_ch(ls, num, 16);
+				_putc('0'); _putc('x');
+				sum += 2;
+				for(int j = 0; j < strlen(ls); j++) _putc(ls[j]);
+				sum+=strlen(ls);
+				i += 2;
+		} break;
+		default: {
+			i = len;
+		}
+	  } 
+	}
+    else {
+      	_putc(fmt[i]);
+		sum++;  	
+      	i++;
+  	}
+ }
+  va_end(ap);
+  return sum; 
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-	char ch;
-  	struct param p;
-	char bf[12]; /*for 32bit number is enough*/
-	
-	p.bf = bf;
-	p.base = 10;
-	p.uc = 0;
-
-	while((ch = *(fmt++))) {
-		if (ch != '%') {
-			chartobuf(out, ch);
-		} else {
-			ch = *(fmt++);
-			switch(ch) {
-				case 's':
-					strtobuf(out, va_arg(ap, char *));						
-					break;
-				case 'd':
-					i2a(va_arg(ap, int), &p);				    
-					strtobuf(out, p.bf);
-			 		break;	
-				default: assert(0);
-			}
-		}
-	}
   return 0;
 }
 
+
 int sprintf(char *out, const char *fmt, ...) {
-  
-    va_list aptr;
-	int ret;
-	
-	va_start(aptr, fmt);
-	ret = vsprintf(out, fmt, aptr);
-	va_end(aptr);
-	return ret;
+  va_list ap;
+  va_start(ap,fmt);
+  int sum=0;
+  int i=0;
+  int len=strlen(fmt);
+  out[0]='\0';
+  while(i<len) {
+    if(fmt[i]=='%') {
+      int num=0;
+      char ls[50]="";
+      switch(fmt[i+1]) {
+        case 'd': {
+	  		num=va_arg(ap,int);
+	  		get_ch(ls, num, 10);
+	  		strcat(out,ls);
+	  		sum+=strlen(ls);	
+        } break;
+		case 's': {
+	  		strcpy(ls,va_arg(ap, char*));
+	  		strcat(out,ls);
+	  		sum += strlen(ls);
+		} break;
+		default: {
+			printf("sprintf fault \n");
+		}
+	  } 
+      i+=2;
+	}
+    else {
+      	sum++;
+      	char s[2];
+      	s[0]=fmt[i];
+      	s[1]='\0';
+      	strcat(out,s);
+      	i++;
+  	}
+ }
+  va_end(ap);
+  return sum; 
 }
 
 /* snprintf()用于将格式化的数据写入字符串 
