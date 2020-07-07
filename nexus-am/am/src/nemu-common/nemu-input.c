2b37345f01cd0b99237602319c88b5adb98dd82f
tracer-ics2019 <tracer@njuics.org>
2020-07-07 20:33:38 +0800

    >  run 181220000 likai Linux likai 5.3.0-62-generic #56~18.04.1-Ubuntu SMP Wed Jun 24 16:17:03 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  20:33:38 up 14:31,  1 user,  load average: 1.01, 1.02, 0.73 2774580ce6ecf75d4aa216c53f1229b3a45bcd17

--- nexus-am/am/src/nemu-common/nemu-input.c
+++ nexus-am/am/src/nemu-common/nemu-input.c
@@ -8,8 +8,10 @@ size_t __am_input_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_INPUT_KBD: {
       _DEV_INPUT_KBD_t *kbd = (_DEV_INPUT_KBD_t *)buf;
-      kbd->keydown = 0;
-      kbd->keycode = _KEY_NONE;
+      int k = inl(KBD_ADDR);
+
+      kbd->keydown = (k&KEYDOWN_MASK ? 1 : 0);
+      kbd->keycode = k & ~KEYDOWN_MASK;
       return sizeof(_DEV_INPUT_KBD_t);
     }
   }
