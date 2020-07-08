b7f2936ec788fd1a35642a1f538ff3badf40dfaf
likai <likmi>
2020-07-08 20:11:15 +0800

    mmio.c

--- nemu/src/device/io/mmio.c
+++ nemu/src/device/io/mmio.c
@@ -5,8 +5,10 @@
 
 static IOMap maps[NR_MAP] = {};
 static int nr_map = 0;
+
 /* 内存映射I/O, 但内存映射I/O的读写并不是面向CPU的，为什么？ */
 /* device interface */
+
 void add_mmio_map(char *name, paddr_t addr, uint8_t* space, int len, io_callback_t callback) {
   assert(nr_map < NR_MAP);
   maps[nr_map] = (IOMap){ .name = name, .low = addr, .high = addr + len - 1,
