82f2056a09b0f17115f01ab52f7c1f44113330d3
likai <likmi>
2020-07-09 14:14:54 +0800

    try to implement VGA

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -30,7 +30,10 @@ static inline void update_screen() {
 
 static void vga_io_handler(uint32_t offset, int len, bool is_write) {
   // TODO: call `update_screen()` when writing to the sync register
-  TODO();
+  // TODO();
+  if (is_write) {
+    update_screen();
+  }
 }
 
 void init_vga() {
