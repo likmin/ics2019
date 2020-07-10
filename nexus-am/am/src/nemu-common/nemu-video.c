29174717b510ecf12118108d47055f017d66456d
tracer-ics2019 <tracer@njuics.org>
2020-07-11 07:57:13 +0800

    >  run 181220000 likai Linux likai 5.3.0-62-generic #56~18.04.1-Ubuntu SMP Wed Jun 24 16:17:03 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  07:57:13 up 12:17,  1 user,  load average: 0.21, 0.06, 0.02 e7b620e6ed0d632227815a76f25751eb1d8156cd

--- nexus-am/am/src/nemu-common/nemu-video.c
+++ nexus-am/am/src/nemu-common/nemu-video.c
@@ -10,8 +10,9 @@ size_t __am_video_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_INFO: {
       _DEV_VIDEO_INFO_t *info = (_DEV_VIDEO_INFO_t *)buf;
-      info->width = 0;
-      info->height = 0;
+      uint32_t screen_info = inl(SCREEN_ADDR);
+      info->width = screen_info>>16;
+      info->height = screen_info&0x0000ffff;
       return sizeof(_DEV_VIDEO_INFO_t);
     }
   }
@@ -22,16 +23,16 @@ size_t __am_video_write(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_FBCTL: {
       _DEV_VIDEO_FBCTL_t *ctl = (_DEV_VIDEO_FBCTL_t *)buf;
-      int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
-      uint32_t *pixels = ctl->pixels;
-      int W = screen_width();
-      int H = screen_height();
-      int cp_bytes = sizeof(uint32_t) * min(w, W - x);
-      uint32_t *vmem = (uint32_t *)(uintptr_t)FB_ADDR;
-      for (int j = 0; j < h && y + j < H; j++){
-        memcpy(&vmem[(y + j) * W + x], pixels, cp_bytes);
-        pixels += w;
-      }
+      // int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+      // uint32_t *pixels = ctl->pixels;
+      // int W = screen_width();
+      // int H = screen_height();
+      // int cp_bytes = sizeof(uint32_t) * min(w, W - x);
+      // uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+      // for (int j = 0; j < h && y + j < H; j++){
+      //  // memcpy(&fb[(y + j) * W + x], pixels, cp_bytes);
+      //   pixels += w;
+      // }
       if (ctl->sync) {
         outl(SYNC_ADDR, 0);
       }
