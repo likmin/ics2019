c508ea5747fef63f182fa3b199e2018fc5ae0960
tracer-ics2019 <tracer@njuics.org>
2020-07-11 08:05:08 +0800

    >  compile 181220000 likai Linux likai 5.3.0-62-generic #56~18.04.1-Ubuntu SMP Wed Jun 24 16:17:03 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  08:05:08 up 12:25,  1 user,  load average: 0.38, 0.27, 0.11 814168c84b37c37870651096ed2d305fa2d4e2d7

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -64,7 +64,7 @@ static void vga_io_handler(uint32_t offset, int len, bool is_write) {
   // TODO();
   switch (offset) {
     case 0: assert(is_write == false); return;
-    case 4: if(is_write) update_screen(); break;
+    //case 4: if(is_write) update_screen(); break;
     default:assert(0);
   }
 }
