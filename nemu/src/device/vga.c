571c3550e0af9466ba16cf36cf3077a75a846d8c
tracer-ics2019 <tracer@njuics.org>
2020-07-11 09:03:20 +0800

    >  compile 181220000 likai Linux likai 5.3.0-62-generic #56~18.04.1-Ubuntu SMP Wed Jun 24 16:17:03 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  09:03:20 up 13:23,  1 user,  load average: 0.21, 0.08, 0.16 8180b0b19c694dee718d2afcaced7f56542d84a2

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -49,9 +49,6 @@ static inline void update_screen() {
    *  4.将新获得的renderer显示出来
    * TODO: 屏幕一片黑，是不是vmem没有数据？
    */
-  //int i;
-  //for (i = 0; i < SCREEN_H; i++)
-    //memset(vmem[i], SCREEN_W, 0xFFFFFFFF);
   SDL_UpdateTexture(texture, NULL, vmem, SCREEN_W * sizeof(vmem[0][0])); 
   SDL_RenderClear(renderer);    /* clear the current rendering target with drawing color */
   SDL_RenderCopy(renderer, texture, NULL, NULL);  /* copy a portion of the texture to the current rendering target */
