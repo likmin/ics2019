e9b7aa2f190c27aa5abbe66a498cf639bc59ff91
tracer-ics2019 <tracer@njuics.org>
2020-07-11 08:07:58 +0800

    >  run 181220000 likai Linux likai 5.3.0-62-generic #56~18.04.1-Ubuntu SMP Wed Jun 24 16:17:03 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  08:07:58 up 12:28,  1 user,  load average: 0.34, 0.30, 0.14 37590e6a894996f2db75eadb15a30344f1d15fc

--- nexus-am/am/src/nemu-common/nemu-video.c
+++ nexus-am/am/src/nemu-common/nemu-video.c
@@ -23,16 +23,16 @@ size_t __am_video_write(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_FBCTL: {
       _DEV_VIDEO_FBCTL_t *ctl = (_DEV_VIDEO_FBCTL_t *)buf;
-      // int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
-      // uint32_t *pixels = ctl->pixels;
-      // int W = screen_width();
-      // int H = screen_height();
-      // int cp_bytes = sizeof(uint32_t) * min(w, W - x);
-      // uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-      // for (int j = 0; j < h && y + j < H; j++){
-      //   memcpy(&fb[(y + j) * W + x], pixels, cp_bytes);
-      //   pixels += w;
-      // }
+      int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+      uint32_t *pixels = ctl->pixels;
+      int W = screen_width();
+      int H = screen_height();
+      int cp_bytes = sizeof(uint32_t) * min(w, W - x);
+      uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+      for (int j = 0; j < h && y + j < H; j++){
+        memcpy(&fb[(y + j) * W + x], pixels, cp_bytes);
+        pixels += w;
+      }
       if (ctl->sync) {
         outl(SYNC_ADDR, 0);
       }
