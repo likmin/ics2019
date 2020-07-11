f2cdfcf0a8f1cc1f039ac6e4961915ab1a4ff85c
likai <likmi>
2020-07-11 08:13:08 +0800

    实现屏幕显示动画

--- nexus-am/am/src/nemu-common/nemu-video.c
+++ nexus-am/am/src/nemu-common/nemu-video.c
@@ -11,8 +11,10 @@ size_t __am_video_read(uintptr_t reg, void *buf, size_t size) {
     case _DEVREG_VIDEO_INFO: {
       _DEV_VIDEO_INFO_t *info = (_DEV_VIDEO_INFO_t *)buf;
       uint32_t screen_info = inl(SCREEN_ADDR);
+
       info->width = screen_info >> 16;
       info->height = screen_info & 0x0000ffff;
+      
       return sizeof(_DEV_VIDEO_INFO_t);
     }
   }
@@ -23,6 +25,7 @@ size_t __am_video_write(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_FBCTL: {
       _DEV_VIDEO_FBCTL_t *ctl = (_DEV_VIDEO_FBCTL_t *)buf;
+
       int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
       uint32_t *pixels = ctl->pixels;
       int W = screen_width();
@@ -33,6 +36,7 @@ size_t __am_video_write(uintptr_t reg, void *buf, size_t size) {
         memcpy(&fb[(y + j) * W + x], pixels, cp_bytes);
         pixels += w;
       }
+     
       if (ctl->sync) {
         outl(SYNC_ADDR, 0);
       }
@@ -43,9 +47,9 @@ size_t __am_video_write(uintptr_t reg, void *buf, size_t size) {
 }
 
 void __am_vga_init() {
-  // int i;
-  // int size = screen_width() * screen_height();
-  // uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  // for (i = 0; i < size; i++) fb[i] = i;
-  // draw_sync();
+  int i;
+  int size = screen_width() * screen_height();
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  for (i = 0; i < size; i++) fb[i] = i;
+  draw_sync();
 }
