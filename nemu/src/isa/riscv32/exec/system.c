39d6e8726d19d1284bcd8a411e8419f4a5c6474f
likai <likmi>
2020-07-13 11:21:22 +0800

    inplement system instruction

--- nemu/src/isa/riscv32/exec/system.c
+++ nemu/src/isa/riscv32/exec/system.c
@@ -14,3 +14,70 @@ make_EHelper(ECALL_EBREAK) { /* void exec_ECALL_EBREAK */
 
     } else assert(0);
 }
+
+make_EHelper(CSRRW) {
+  /* t        = CSR[csr]
+   * CSR[csr] = x[rs1]
+   * x[rd]    = t
+   */
+  t0 = csr_read(id_src2->val);
+  csr_write(id_src2->val, id_src);
+  rtl_sr(id_dest->reg, &t0, 4);
+
+}
+
+make_EHelper(CSRRS) {
+  /* t        = CSR[csr]
+   * CSR[csr] = t | x[rs1]
+   * x[rd]    = t
+   */
+  t0 = csr_read(id_src2->val);
+  rtl_or(&t1, &t0, &id_src->val);
+  csr_write(id_src2->val, t1);
+  rtl_sr(id_dest->reg, &t0, 4);
+}
+
+make_EHelper(CSRRC) {
+   /* t        = CSR[csr]
+   * CSR[csr] = t & ~x[rs1]
+   * x[rd]    = t
+   */
+  t0 = csr_read(id_src2->val); /* t0 = CSR[csr] */
+  rtl_not(&s0, &id_src->val);  /* s0 = ~x[rs1]  */
+  rtl_and(&t1, &t0, &s0);       /* t1 = t0 | s0  */
+  csr_write(id_src2->val, t1); /* CSR[csr] = t1 */
+  rtl_sr(id_dest->reg, &t0, 4);/* x[rd] = t0    */
+}
+
+make_EHelper(CSRRWI) {
+ /* x[rd] = CSR[csr]
+  * CSR[csr] = zimm
+  */
+
+  t0 = csr_read(id_src2->val);
+  rtl_sr(id_dest->reg, &t0, 4);/* x[rd] = CSR[csr] */
+  csr_write(id_src2->val, id_src->reg) /* CSR[csr] = zimm */
+}
+
+make_EHelper(CSRRSI) {
+ /* t = CSR[csr]
+  * CSR[csr] = t | zimm
+  * x[rd] = t
+  */
+
+  t0 = csr_read(id_src2->val);
+  rtl_or(&t1, &t0, &id_src->reg);
+  rtl_sr(id_dest->reg, &t0, 4); 
+}
+
+make_EHelper(CSRRCI) {
+  /* t = CSR[csr]
+   * CSR = t & ~zimm
+   * x[rd] = t
+   */
+
+   t0 = csr_read(id_src2->val);
+   rtl_not(&t1, &id_src->reg);
+   rtl_and(&r0, &t0, &t1);
+   rtl_sr(id_dest->reg, &t0, 4);
+}
--- nemu/src/isa/riscv32/include/isa/reg.h
+++ nemu/src/isa/riscv32/include/isa/reg.h
@@ -15,31 +15,32 @@ typedef struct {
   struct {
 
     struct {
-      uint32_t SD           : 1;
-      uint32_t sstatu_30_20 :11;
-      uint32_t MXR          : 1;
-      uint32_t SUM          : 1;
-      uint32_t sstatus_17   : 1;
-      uint32_t XS           : 2;
-      uint32_t FS           : 2;
-      uint32_t sstatus_12_9 : 4;
-      uint32_t SPP          : 1;
-      uint32_t sstatus_7    : 1;
-      uint32_t UBE          : 1;
-      uint32_t SPIE         : 1;
-      uint32_t sstatus_4_2  : 3;
-      uint32_t SIE          : 1;
       uint32_t sstatus_0    : 1;
+      uint32_t SIE          : 1;
+      uint32_t sstatus_4_2  : 3;
+      uint32_t SPIE         : 1;
+      uint32_t UBE          : 1;
+      uint32_t sstatus_7    : 1;
+      uint32_t SPP          : 1;
+      uint32_t sstatus_12_9 : 4;
+      uint32_t FS           : 2;
+      uint32_t XS           : 2;
+      uint32_t sstatus_17   : 1;
+      uint32_t SUM          : 1;
+      uint32_t MXR          : 1;
+      uint32_t sstatu_30_20 :11;
+      uint32_t SD           : 1;
     } sstatus;
 
     uint32_t sepc;
     
     struct {
-      uint32_t Interrupt    : 1;
       uint32_t Ecode        :31;
+      uint32_t Interrupt    : 1;
     } scause;
 
-  } CSR;
+  } csr;
+
 } CPU_state;
 
 static inline int check_reg_index(int index) {
@@ -55,6 +56,28 @@ static inline const char* reg_name(int index, int width) {
   return regsl[index];
 }
 
+static inline void check_csr_index(int index) {
+  assert(index >= 0 && index <= 0xfff);
+}
+
+inline rtlreg_t csr_read(int index) {
+  check_csr_index(index);
+  switch(index) {
+    case 0x100: return cpu.csr.sstatus;
+    case 0x141: return cpu.csr.sepc;
+    case 0x142: return cpu.csr.scause;
+    default: return;
+  }
+}
+inline void csr_write(int index, rtlreg_t val) {
+  check_csr_index(index);
+  switch(index) {
+    case 0x100: cpu.csr.sstatus = val; return;
+    case 0x141: cpu.csr.sepc    = val; return;
+    case 0x142: cpu.csr.scause  = val; return;
+    default   : assert(0);
+  }
+}
 //#define REG_REGEX "^\\$0$|^\\$ra$|^\\$[sgt]p$|^\\$t[0-6]$|^\\$s[0-9]$|^\\$s1[01]$|^\\$a[0-7]$|^\\$pc$"  
 
 #define REG_REGEX "^\\$0|^\\$ra|^\\$[sgt]p|^\\$t[0-6]|^\\$s[0-9]|^\\$s1[01]|^\\$a[0-7]|^\\$pc"  
