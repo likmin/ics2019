db23453607270b78fd65c34b6b898cf926a59cb9
tracer-ics2019 <tracer@njuics.org>
2020-07-16 10:04:40 +0800

    >  run 181220000 likai Linux likai 5.3.0-62-generic #56~18.04.1-Ubuntu SMP Wed Jun 24 16:17:03 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  10:04:40 up 11:51,  1 user,  load average: 0.00, 0.01, 0.00 861ebfcb4ed85c374a72cee03790f7737ff9a307

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -3,7 +3,7 @@
 static _Context* do_event(_Event e, _Context* c) {
   switch (e.event) {
     case _EVENT_YIELD: _yield(); break;
-    default: panic("Unhandled event ID = %d", e.event);
+    default: printf("e.event = %d\n", e.event);panic("Unhandled event ID = %d", e.event);
   }
 
   return NULL;
