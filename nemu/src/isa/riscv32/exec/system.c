246e5e3419b42f100ed5ad25bdd9f4fc8674369f
tracer-ics2019 <tracer@njuics.org>
2020-07-17 10:59:02 +0800

    >  compile 181220000 likai Linux likai 5.3.0-62-generic #56~18.04.1-Ubuntu SMP Wed Jun 24 16:17:03 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  10:59:02 up 35 min,  1 user,  load average: 0.04, 0.11, 0.09 7a64d2d1faffd8c9801af6176f44b8f8ede6000d

--- nemu/src/isa/riscv32/exec/system.c
+++ nemu/src/isa/riscv32/exec/system.c
@@ -29,7 +29,7 @@ void csr_write(int index, rtlreg_t val) {
 
 make_EHelper(ECALL_EBREAK) { /* void exec_ECALL_EBREAK */
     
-    
+    printf("rd = %d, rs1 = %d\n", decinfo.isa.instr.rd, decinfo.isa.instr.rs1); 
     if (decinfo.isa.instr.rd == 0 && decinfo.isa.instr.rs1 == 0) {
         
         printf("simm11_0 = %d, pc=0x%08x\n", decinfo.isa.instr.simm11_0, decinfo.seq_pc);
