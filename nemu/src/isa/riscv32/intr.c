07f3476b59be5a67b65cd2822ef2045d756e9c70
tracer-ics2019 <tracer@njuics.org>
2020-07-17 15:33:08 +0800

    >  compile 181220000 likai Linux likai 5.3.0-62-generic #56~18.04.1-Ubuntu SMP Wed Jun 24 16:17:03 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:33:08 up  1:36,  1 user,  load average: 0.12, 0.24, 0.21 6586a570bafbedacb4185c1d5f81c19fb78178b1

--- nemu/src/isa/riscv32/intr.c
+++ nemu/src/isa/riscv32/intr.c
@@ -13,8 +13,8 @@ void raise_intr(uint32_t NO, vaddr_t epc) {
 
   csr_write(SEPC, epc);  /* 1.save the pc to epc                */
   csr_write(SCAUSE, NO); /* 2.set the exception code to scause  */
-  vaddr_t tvec = csr_read(STVEC); /* 3.get the exception entry address from stvec register */ 
-  interpret_rtl_j(tvec); /* 4.jump to exception entry address   */
+  t0 = csr_read(STVEC); /* 3.get the exception entry address from stvec register */ 
+  interpret_rtl_jr(&t0); /* 4.jump to exception entry address   */
 }
 
 bool isa_query_intr(void) {
