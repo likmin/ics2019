cdac52f234929d7ec79401bf50cbd297fef7d3ba
tracer-ics2019 <tracer@njuics.org>
2020-07-17 16:30:08 +0800

    >  run 181220000 likai Linux likai 5.3.0-62-generic #56~18.04.1-Ubuntu SMP Wed Jun 24 16:17:03 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:30:08 up  2:33,  1 user,  load average: 0.04, 0.07, 0.09 b5544a754d89c7df7dbf85d99def8939f7ae03c

--- nexus-am/am/src/riscv32/nemu/cte.c
+++ nexus-am/am/src/riscv32/nemu/cte.c
@@ -16,7 +16,7 @@ _Context* __am_irq_handle(_Context *c) {
  // Log("epc = %d, cause = %d, status = %d\n", c->epc, c->cause, c->status);
   if (user_handler) {
     _Event ev = {0};
-    switch (c->cause) {
+    switch (c->scause) {
       case 9 : ev.event = _EVENT_YIELD; break;
       //default: ev.event = _EVENT_YIELD; break;
       default: ev.event = _EVENT_ERROR; break;
