1731e4f9bd86d9d2c1e53f93fe1f9f291f9ff120
tracer-ics2019 <tracer@njuics.org>
2020-07-27 13:34:27 +0800

    >  run 181220000 likai Linux likai 5.4.0-42-generic #46~18.04.1-Ubuntu SMP Fri Jul 10 07:21:24 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  13:34:27 up  7:58,  2 users,  load average: 0.09, 0.03, 0.00 cdaf927bddd12b64407c552378b620b9f503aa6d

--- nanos-lite/src/ramdisk.c
+++ nanos-lite/src/ramdisk.c
@@ -24,12 +24,11 @@ size_t ramdisk_write(const void *buf, size_t offset, size_t len) {
 }
 
 void init_ramdisk() {
-  Log("ramdisk info: start = %p, end = %p, size = %d bytes",
+  Log("ramdisk info: start = %d, end = %p, size = %d bytes",
       &ramdisk_start, &ramdisk_end, RAMDISK_SIZE);
 }
 
 size_t get_ramdisk_size() {
-    Log("ramdisk info: start = %p, end = %p, size = %d bytes",
-      &ramdisk_start, &ramdisk_end, RAMDISK_SIZE);
+
   return RAMDISK_SIZE;
 }
