417b95b034ec8c75ad3ca35e917ea39373e1cc7f
tracer-ics2019 <tracer@njuics.org>
2020-07-29 16:56:15 +0800

    >  run 181220000 likai Linux likai 5.4.0-42-generic #46~18.04.1-Ubuntu SMP Fri Jul 10 07:21:24 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:56:15 up 1 day, 10:14,  2 users,  load average: 1.02, 0.91, 0.92 79c78ee04f079c97bedcf0e1ce22b91050ca0c

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -14,16 +14,15 @@ char *strcpy(char* dst,const char* src) {
 }
 
 char* strncpy(char* dst, const char* src, size_t n) {
-  size_t srcSize = strlen(src) + 1; // for '\0'
   
-  if(n > srcSize) n = srcSize;
-
   uint32_t i;
-  for (i = 0; i < n; i++) {
+  for (i = 0; i < n && src[i]!='\0'; i++) {
 	  dst[i] = src[i];
   }
+ 
+  for (;i<n;i++)
+    dst[i] = '\0';
 
-  dst[i] = '\0';
   return dst;
 }
 
