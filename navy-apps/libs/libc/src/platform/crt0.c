44935e4237ad3e0aae5c2ffb8d3bd50e61ece7c3
tracer-ics2019 <tracer@njuics.org>
2020-07-29 10:58:51 +0800

    >  compile 181220000 likai Linux likai 5.4.0-42-generic #46~18.04.1-Ubuntu SMP Fri Jul 10 07:21:24 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  10:58:51 up 1 day,  6:13,  2 users,  load average: 0.00, 0.03, 0.00 44ffc06e791c42562fc76f2ad29654751f39e5a

--- navy-apps/libs/libc/src/platform/crt0.c
+++ navy-apps/libs/libc/src/platform/crt0.c
@@ -4,9 +4,10 @@
 int main(int argc, char *argv[], char *envp[]);
 extern char **environ;
 
+/*_start(...) 用户程序的入口*/
 void _start(int argc, char *argv[], char *envp[]) {
   char *env[] = {NULL};
   environ = env;
-  exit(main(argc, argv, env));
+  exit(main(argc, argv, env)); /* 从main() 函数返回后会调exit()结束运行 */ 
   assert(0);
 }
