877f3947fbacd0e1243907687b69d1e1b13a4e32
tracer-ics2019 <tracer@njuics.org>
2020-08-16 21:20:03 +0800

    >  compile 181220000 likai Linux likai 5.4.0-42-generic #46~18.04.1-Ubuntu SMP Fri Jul 10 07:21:24 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  21:20:03 up  2:58,  1 user,  load average: 0.17, 0.06, 0.01 31f1e0553cf0367f995ea4bc573adccc106431d6

--- nemu/src/isa/riscv32/exec/system.c
+++ nemu/src/isa/riscv32/exec/system.c
@@ -54,7 +54,8 @@ make_EHelper(ECALL_EBREAK) { /* void exec_ECALL_EBREAK */
         switch (decinfo.isa.instr.simm11_0) {
           case 0b000000000000: /* ECALL */ 
           
-            raise_intr(reg_l(17), cpu.pc + 4); 
+            raise_intr(9, cpu.pc + 4); 
+            printf("cpu.pc = %8x, decinfo.seq_pc = %8x\n", cpu.pc, decinfo.seq_pc);
             print_asm_template1(ecall); 
             break;
           case 0b000000000001: /* EBREAK*/ TODO(); break;
