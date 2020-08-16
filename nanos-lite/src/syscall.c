f177ddb9c282e70f887f7fb4b1f7279afca96828
tracer-ics2019 <tracer@njuics.org>
2020-08-16 23:03:13 +0800

    >  run 181220000 likai Linux likai 5.4.0-42-generic #46~18.04.1-Ubuntu SMP Fri Jul 10 07:21:24 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  23:03:13 up  4:41,  1 user,  load average: 0.24, 0.07, 0.08 2d056933a56bb57825b23fc0b7837f4084ca2aaf

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -6,7 +6,7 @@ _Context* do_syscall(_Context *c) {
   a[0] = c->GPR1;
   //printf("do_syscall\n");
   switch (a[0]) {
-    //case SYS_yield:  _yield();c->GPRx = 0; break;
+    case SYS_yield:  _yield();c->GPRx = 0; break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
