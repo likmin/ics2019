4c8c2dafea60c9baa761a54ab9cbfec8479ccaf1
tracer-ics2019 <tracer@njuics.org>
2020-08-16 21:50:39 +0800

    >  compile 181220000 likai Linux likai 5.4.0-42-generic #46~18.04.1-Ubuntu SMP Fri Jul 10 07:21:24 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  21:50:39 up  3:29,  1 user,  load average: 0.11, 0.09, 0.10 b7fbc04244ab33ba3467f31a9b822c9ee278b3b2

--- nemu/src/isa/riscv32/intr.c
+++ nemu/src/isa/riscv32/intr.c
@@ -11,6 +11,11 @@ void raise_intr(uint32_t NO, vaddr_t epc) {
    * 4. jump to exception entry address 
    */
 
+  /* 你可能会问：stvec的值是从哪里来的？
+   * stvec寄存器用来存放异常入口地址的，也就是__am_asm_trap的地址，
+   * 在操作系统初始化时，_cte_init()函数中就已经将__am_asm_trap的
+   * 地址通过内敛函数传递给了stvec。
+   */
   csr_write(SEPC, epc);  /* 1.save the pc to epc                */
   csr_write(SCAUSE, NO); /* 2.set the exception code to scause  */
   t0 = csr_read(STVEC);  /* 3.get the exception entry address from stvec register */ 
