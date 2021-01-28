4bd3c0b0864a60059de8da1e37a491da32cf375a
tracer-ics2019 <tracer@njuics.org>
2021-01-27 16:53:12 -0800

    >  compile 181220000 likai Linux ubuntu 5.8.0-38-generic #43~20.04.1-Ubuntu SMP Tue Jan 12 16:39:47 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  16:53:12 up 55 min,  1 user,  load average: 0.22, 0.17, 0.14 252570cd95411cc2daa9658325bec366d4b33056

--- init.sh
+++ init.sh
@@ -5,16 +5,16 @@ version=ics2019
 log=""
 
 function init() {
-#  if [ -d $1 ]; then
-#    echo "$1 is already initialized, exiting..."
-#    return
-#  fi
-#
-#  while [ ! -d $1 ]; do
-#    git clone -b $version https://github.com/NJU-ProjectN/$1.git
-#  done
-#  log="$log$1 `cd $1 && git log --oneline --no-abbrev-commit -n1`"$'\n'
-#  rm -rf $1/.git
+  if [ -d $1 ]; then
+    echo "$1 is already initialized, exiting..."
+    return
+  fi
+
+  while [ ! -d $1 ]; do
+    git clone -b $version https://github.com/NJU-ProjectN/$1.git
+  done
+  log="$log$1 `cd $1 && git log --oneline --no-abbrev-commit -n1`"$'\n'
+  rm -rf $1/.git
 
   if [ $2 ] ; then
     sed -i -e "/^export $2=.*/d" ~/.bashrc
