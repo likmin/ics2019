4b50eba4ba8aa77bf07da1b932d60a4c7eea9503
tracer-ics2019 <tracer@njuics.org>
2021-01-27 16:54:43 -0800

    >  compile 181220000 likai Linux ubuntu 5.8.0-38-generic #43~20.04.1-Ubuntu SMP Tue Jan 12 16:39:47 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  16:54:43 up 57 min,  1 user,  load average: 0.13, 0.14, 0.12 5f54f91e38332d9a3b52ddc7e8ff4623bb769f8

--- nemu/include/common.h
+++ nemu/include/common.h
@@ -1,8 +1,8 @@
 #ifndef __COMMON_H__
 #define __COMMON_H__
 
-//#define DEBUG
-//#define DIFF_TEST
+#define DEBUG
+#define DIFF_TEST
 
 #if _SHARE
 // do not enable these features while building a reference design
