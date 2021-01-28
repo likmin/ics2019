37655173ba54f26908c1ce8d14d0eb8b2c3fc9d0
likai <likmi@qq.com>
2021-01-28 06:05:11 -0800

    add reinit.sh, instools.sh

---
+++ instools.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt-get install build-essential    # build-essential packages, include binary utilities, gcc, make, and so on
+sudo apt-get install man                # on-line reference manual
+sudo apt-get install gdb                # GNU debugger
+sudo apt-get install git                # reversion control system
+sudo apt-get install libreadline-dev    # a library to use compile the project later
+sudo apt-get install libsdl2-dev        # a library to use compile the project later
+sudo apt-get install libc6-dev-i386     # a library to use compile the project later
+sudo apt-get install qemu-system        # QEMU
