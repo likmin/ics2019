017c24d28f4c8eb565b33f97d8a128de504490e1
tracer-ics2019 <tracer@njuics.org>
2021-01-30 06:03:25 -0800

    >  compile 181220000 likai Linux ubuntu 5.8.0-38-generic #43~20.04.1-Ubuntu SMP Tue Jan 12 16:39:47 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  06:03:25 up 36 min,  1 user,  load average: 0.24, 0.10, 0.09 77295ed91cc8b29fe44d167bc0284e8e342360f4

--- nemu/src/isa/x86/include/isa/reg.h
+++ nemu/src/isa/x86/include/isa/reg.h
@@ -17,7 +17,8 @@ enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
  */
 
 typedef struct {
-  struct {
+union{
+   union {
     uint32_t _32;
     uint16_t _16;
     uint8_t _8[2];
@@ -29,7 +30,7 @@ typedef struct {
    * in PA2 able to directly access these registers.
    */
   rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
-
+};
   vaddr_t pc;
 
 } CPU_state;
