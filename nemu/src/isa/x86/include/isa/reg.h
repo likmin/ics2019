59cb430e16ba8a7b9bcb2eadf8310d572d8db2b7
tracer-ics2019 <tracer@njuics.org>
2021-01-30 06:10:16 -0800

    >  compile 181220000 likai Linux ubuntu 5.8.0-38-generic #43~20.04.1-Ubuntu SMP Tue Jan 12 16:39:47 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  06:10:16 up 43 min,  1 user,  load average: 0.05, 0.10, 0.09 cc7770963c5ccc4d6095f28569f0d886e3e3f833

--- nemu/src/isa/x86/include/isa/reg.h
+++ nemu/src/isa/x86/include/isa/reg.h
@@ -29,7 +29,7 @@ union{
   /* In NEMU, rtlreg_t is exactly uint32_t. This makes RTL instructions
    * in PA2 able to directly access these registers.
    */
-  rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+ struct{ rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;};
 };
   vaddr_t pc;
 
